133b4680254c2ff82649f306f9bbfce7236e25a3
Nazarii Plebanskyi <nazar796@gmail.com>
2018-02-26 13:24:35 +0200

    Added .bat file for quick setup.

---
+++ INSTALL_VENV_AND_FLASK.bat
@@ -0,0 +1,10 @@
+REM installing virtualenv on your computer
+pip install virtualenv
+REM setting up virtualev in venv folder
+virtualenv venv
+REM activating virtual environment
+venv\Scripts\activate
+REM installing Flask to run local server
+pip install Flask
+REM updating setuptools to the latest version
+pip install --upgrade pip setuptools
